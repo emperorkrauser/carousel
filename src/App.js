0d88bc8d56d5e1b56c8927eae193d694c634bc10
Christian Adam Rosell <krauserchristian@gmail.com>
2019-02-12 00:47:13 +0800

    hide carousel items

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles/App.scss';
+import carouselItems from "./constants/carousel_items";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      item: "",
+      active: true
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      items: carouselItems,
+    }, () => {
+      this.checkCarousel();
+    });
+    
+  }
+
+  checkCarousel(){
+    if(this.state.items){
+      const carousel = document.querySelectorAll(".carousel-item");
+      console.log(carousel);
+    }
+  }
+
   render() {
+    const {items} = this.state;
+    let newItems;
+    if(items){
+      newItems = items.map( (item) => {
+        return(
+          <div key={item.id} className="carousel-item">
+            <div className="img-container">
+              <img src={item.image_url} alt=""/>
+            </div>
+            <div className="img-description">
+              <p>{item.img_description}</p>
+            </div>
+          </div>
+        )
+      });
+    }
+
     return (
       <div className="App">
         <div className="sidebar"></div>
         <header className="">
           <div className="carousel-container">
-            <div className="carousel-item">
-              <div className="img-container">
-                <img src="https://via.placeholder.com/640x360" alt=""/>
-              </div>
-              <div className="img-description">
-                <p>This is a description</p>
-              </div>
-            </div>
+            {newItems}
             <div className="carousel-buttons">
               <button className="left">Prev</button>
               <button className="right">Next</button>
---
+++ src/constants/carousel_items.js
@@ -0,0 +1,22 @@
+const carouselItems = [
+	{
+		id:1, 
+    image_url: "https://via.placeholder.com/640x360",
+    img_description: "This is the first description",
+    active: true,
+	},
+  {
+    id:2, 
+    image_url: "https://via.placeholder.com/640x360",
+    img_description: "This is a second description",
+    active: false,
+  },
+  {
+    id:3, 
+    image_url: "https://via.placeholder.com/640x360",
+    img_description: "This is a third description",
+    active: false,
+  },
+];
+
+export default carouselItems;
